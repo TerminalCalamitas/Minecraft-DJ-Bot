ce3231d9911282c702552911d93b23dda217bcca
caleb <paracordcaleb@gmail.com>
2024-03-04 20:45:27 -0500

    Changed command name to make more sense to other people

--- commands/calamity/admin-leave.js
+++ commands/calamity/admin-leave.js
@@ -1,19 +1,19 @@
-var name = "bananaboo"
-var aliases = ["bananababoo", "bboo"]
-var description = "Who knows?"
-var usage = "{prefix}bananaboo"
+var name = 'admin'
+var aliases = ['panic', 'scram']
+var description = 'Who knows?'
+var usage = '{prefix}admin'
 var enabled = true
 var hidden = false
 
-var util = require("./../../util.js")
-var perms = require("./../../config.json").commands_perms
+var util = require('./../../util.js')
+var perms = require('./../../config.json').commands_perms
 
 
 function execute(bot, cmd, username, args, handler) {
     if (!perms.includes(username)) {
-        return bot.chat(util.errorMessage("No permisson to use this command."))
+        return bot.chat(util.errorMessage('No permisson to use this command.'))
     }
-    bot.chat("^AAAHHHH I HAVE TO STAY SAFE!!!!")
+    bot.chat('^AAAHHHH I HAVE TO STAY SAFE!!!!')
     process.exit()
 }
 
