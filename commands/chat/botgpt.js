37a7970742af6d39c46c98167de9a126ba4be865
caleb <paracordcaleb@gmail.com>
2024-03-06 12:11:52 -0500

    Removed unnecessary console log

--- commands/chat/botgpt.js
+++ commands/chat/botgpt.js
@@ -52,7 +52,6 @@ async function execute(bot, cmd, username, args, handler) {
             })
 
             // Gets the chatgpt response from returned json format
-            console.log(response)
             answer = response.choices[0].message.content
             // Bot says message then changes it so that it is no longer gererating a response
             bot.chat(answer)
